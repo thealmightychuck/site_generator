abec540935d987791c7ca9b165f6cb22979f2cd4
Charles Moore <chuckm224@gmail.com>
2026-05-02 13:32:43 -0500

    added docs directory and changed base url

--- src/gencontent.py
+++ src/gencontent.py
@@ -8,7 +8,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("no h1 found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(from_path, "r") as file:
@@ -23,18 +23,21 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(content)
 
     final_html = template.replace("{{ Content }}",html_string).replace("{{ Title }}", title)
+    final_html = final_html.replace('href="/', 'href="' + basepath)
+    final_html = final_html.replace('src="/', 'src="' + basepath)
+    
     with open(dest_path, 'w') as file:
         file.write(final_html)
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         if Path(entry).suffix == ".md":
             source_path = Path(os.path.join(dir_path_content, entry))
             new_path = Path(os.path.join(dest_dir_path, entry))
             new_path = new_path.with_suffix(".html")
-            generate_page(source_path, template_path, new_path)
+            generate_page(source_path, template_path, new_path, basepath)
         if Path(os.path.join(dir_path_content, entry)).is_dir():
             new_dir_path = os.path.join(dir_path_content, entry)
             new_dest_dir_path = os.path.join(dest_dir_path, entry)
-            generate_pages_recursive(new_dir_path, template_path, new_dest_dir_path)
+            generate_pages_recursive(new_dir_path, template_path, new_dest_dir_path, basepath)
 
--- src/main.py
+++ src/main.py
@@ -2,19 +2,24 @@ import os
 import shutil
 from copystatic import copy_files_recursive
 from gencontent import extract_title, generate_page, generate_pages_recursive
+import sys
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 template = "./content/index.md"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory")
     copy_files_recursive(dir_path_static, dir_path_public)
-    #generate_page("./content/index.md", "./template.html", "./public/index.html")
-    generate_pages_recursive("./content/", "./template.html", "./public/")
+    generate_pages_recursive("./content/", "./template.html", "./public/", basepath)
 
 main()
